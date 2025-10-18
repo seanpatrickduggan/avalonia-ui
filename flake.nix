dfa66c607906db7cd09ae35807050b07525a60b9
Sean Duggan <sean.duggan.cs@gmail.com>
2025-10-17 23:48:44 -0400

    Add flake.nix and flake.lock for .NET Avalonia development environment

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A .NET Avalonia development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./shell.nix {};
+  };
+}
